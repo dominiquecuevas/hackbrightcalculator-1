17648059d2ff2f41a5e2ff79cbcd15af235bbc7c
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-02 12:40:36 -0700

    Modified subtract and multiply functions.

--- arithmetic.py
+++ arithmetic.py
@@ -9,15 +9,15 @@ def add(num1, num2):
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
-
+    return num1 - num2
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
-
+    return num1 * num2
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
-
+    
 
 def square(num1):
     """Return the square of the input."""
